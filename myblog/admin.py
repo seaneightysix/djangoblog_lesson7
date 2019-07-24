9d9282e75ef1e3e61430dc997d1faaeccde4d3f5
seaneightysix <sean.eightysix@GMAIL.com>
2019-07-24 17:28:34 -0500

    completed

--- myblog/admin.py
+++ myblog/admin.py
@@ -1,17 +1,6 @@
 from django.contrib import admin
-from myblog.models import Post, Genre
+from myblog.models import Genre
+from myblog.models import Post
 
-class GenreInline(admin.TabularInline):
-    model = Genre.posts.through
-
-class PostAdmin(admin.ModelAdmin):
-    inlines = [
-        GenreInline,
-    ]
-    
-class GenreAdmin(admin.ModelAdmin):
-    exclude = ('posts',)
-
-admin.site.register(Post, PostAdmin)
-
-admin.site.register(Genre, GenreAdmin)
+admin.site.register(Genre)
+admin.site.register(Post)
